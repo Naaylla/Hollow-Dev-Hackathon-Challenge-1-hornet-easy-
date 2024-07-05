18bc181c72edd7267bf311b429719bcb4135dbba
Naaylla <lulita181910@hotmail.fr>
2024-07-05 16:41:48 +0100

    added DB connection

---
+++ models/fileModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const fileSchema = new mongoose.Schema({
+    name: String,
+    path: String,
+    size: Number,
+    uploadDate: { type: Date, default: Date.now }
+});
+
+const File = mongoose.model('File', fileSchema);
